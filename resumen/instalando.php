4396f19effc6971a81c789f6a3ea5932281d30ba
oscar <oscarcased2017@gmail.com>
2017-09-12 17:00:53 -0500

    actualizando variables entornos

--- resumen/instalando.php
+++ resumen/instalando.php
@@ -13,6 +13,23 @@ ionic cordova build android
 ./studio.sh
 ///////////////////////////////////
 
+#npm install @ionic-native/core --save
+#ionic cordova plugin add cordova-plugin-camera
+
+
+ionic cordova plugin add phonegap-plugin-barcodescanner 
+sudo npm install --save @ionic-native/barcode-scanner
+
+
+ionic cordova plugin add cordova-plugin-inappbrowser
+sudo npm install --save @ionic-native/in-app-browser
+
+sudo npm install @agm/core --save
+
+
+ionic cordova plugin add cordova-plugin-contacts
+npm install --save @ionic-native/contacts
+
 
 
 
@@ -21,24 +38,23 @@ https://www.qrcode.es/es/generador-qr-code/
 ///////////////////////////////////////////
 
 sudo nano /etc/profile
-JAVA_HOME=/usr/lib/jvm/java-8-oracle 
-JRE_HOME=$JAVA_HOME/jre 
-ANDROID_MIO =/home/estrategas/Android/Sdk 
-ANDROID_HOME =/usr/local/android-studio 
 
-PATH=$PATH:$ANDROID_MIO
-PATH=$PATH:$ANDROID_MIO/bin 
-PATH=$PATH:$ANDROID_MIO/tools 
-PATH=$PATH:$ANDROID_MIO/platform-tools 
-PATH=$PATH:$ANDROID_MIO/tools/bin 
-PATH=$PATH:$ANDROID_HOME
-PATH=$PATH:$ANDROID_HOME/bin 
-PATH=$PATH:$ANDROID_HOME/gradle 
-PATH=$PATH:$ANDROID_HOME/gradle/gradle-3.2/bin/ 
-PATH=$PATH:$JAVA_HOME/bin:$JRE_HOME/bin 
+JAVA_HOME=/usr/lib/jvm/java-8-oracle
+JRE_HOME=$JAVA_HOME/jre
+ANDROID_HOME=/home/estrategas/Android/Sdk
+ANDROID_MIO=/usr/local/android-studio
+PATH=$PATH:$ANDROID_HOME/bin
+PATH=$PATH:$ANDROID_HOME/tools
+PATH=$PATH:$ANDROID_HOME/platform-tools
+PATH=$PATH:$ANDROID_HOME/tools/bin
+PATH=$PATH:$ANDROID_MIO/bin
+PATH=$PATH:$ANDROID_MIO/gradle
+PATH=$PATH:$ANDROID_MIO/gradle/gradle-3.2/bin/
+PATH=$PATH:$JAVA_HOME/bin:$JRE_HOME/bin
 
-export JAVA_HOME 
-export JRE_HOME 
-export ANDROID_MIO 
-export ANDROID_ HOME 
-export PATH 
+export JAVA_HOME
+export JRE_HOME
+export ANDROID_MIO
+export ANDROID_HOME
+export PATH
+
